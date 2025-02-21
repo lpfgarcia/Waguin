339f7ee9e6da913c53ad7d24634451efa95d81ed
lpfgarcia <lpfgarcia@gmail.com>
2025-02-21 15:36:23 -0300

    update

--- rag.py
+++ rag.py
@@ -1,34 +1,82 @@
-import os
+import os, ollama
 import chainlit as cl
 
-from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_community.document_loaders.csv_loader import CSVLoader
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 
+from openai import OpenAI
+
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+# Configurações
 os.environ['OPENAI_API_KEY'] = ''
+client = OpenAI()
+PATH = 'data'
 
-loader = CSVLoader('noticias.csv')
-text = loader.load()
-text_content = '\n'.join([doc.page_content for doc in text])
+TEMPLATE = """
+Meu nome é Waguin. Sou um assistente virtual da Fiocruz.
+Responda de maneira clara, objetiva. Se não souber a resposta, diga que não sabe. Responda sempre em Português.
+Contexto:
+{context}
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-texts = text_splitter.split_text(text_content)
+Pergunta do usuário:
+{question}
+"""
 
-metadatas = [{'source': f'{i}-pl'} for i in range(len(texts))]
+prompt = PromptTemplate(template=TEMPLATE, input_variables=["context", "question"])
 
+def load_csv_data(folder_path):
+    text_content = ''
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith('.csv'):
+            file_path = os.path.join(folder_path, file_name)
+            loader = CSVLoader(file_path)
+            documents = loader.load()
+            text_content += '\n'.join([doc.page_content for doc in documents])
 
-embeddings = OpenAIEmbeddings()
-vectorstore = FAISS.from_texts(texts, OpenAIEmbeddings(), metadatas=metadatas)
-vectorstore.save_local('vectorstore')
+    return text_content
+
+def gpt_response(model, prompt):
+    response = client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": prompt}],
+        )
+    return response.choices[0].message.content
+
+def llama3_response(model, prompt):
+    response = ollama.chat(
+        model='llama3.2:3b', 
+        messages=[{'role': 'user', 'content': prompt}],
+        )
+    return response['message']['content']
+
+def gpt_chain(model, retriever, memory):
+    chain = ConversationalRetrievalChain.from_llm(
+        ChatOpenAI(model_name=model),
+        chain_type='stuff',
+        retriever=retriever,
+        memory=memory,
+        combine_docs_chain_kwargs={"prompt": prompt},
+        verbose=True)
+    return chain
+
+def llama_chain(model, retriever, memory):
+    chain = ConversationalRetrievalChain.from_llm(
+        ChatOllama(model=model),
+        chain_type='stuff',
+        retriever=retriever,
+        memory=memory,
+        combine_docs_chain_kwargs={"prompt": prompt},
+        verbose=True)
+    return chain
 
 @cl.set_chat_profiles
 async def chat_profile():
-
     return [
         cl.ChatProfile(
             name='GPT-3.5',
@@ -40,59 +88,55 @@ async def chat_profile():
             markdown_description='Preciso e criativo para tarefas complexas.',
             icon='https://picsum.photos/250',
         ),
+        cl.ChatProfile(
+            name='Llama3',
+            markdown_description='Modelo open-source otimizado para velocidade e custo-benefício.',
+            icon='https://picsum.photos/300',
+        ),
     ]
 
 @cl.on_chat_start
 async def on_chat_start():
 
-    cl.Message(content="Message 1")
     chat_profile = cl.user_session.get('chat_profile')
     await cl.Message(
-        content=f'iniciando o chat usando o perfil {chat_profile}'
+        content=f'Iniciando o chat usando o modelo {chat_profile} ...\nAgora você pode fazer perguntas!'
     ).send()
-
-    if chat_profile == 'GPT-4':
-        model_name = 'gpt-4'
-    else:
-        model_name = 'gpt-3.5-turbo'
 
     embeddings = OpenAIEmbeddings()
     vectorstore = FAISS.load_local('vectorstore', embeddings, allow_dangerous_deserialization=True)
-    retriever = vectorstore.as_retriever()
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
 
-    message_history = ChatMessageHistory()
     memory = ConversationBufferMemory(
         memory_key='chat_history',
         return_messages=True,
-        output_key='answer'
+        output_key='answer',
     )
 
-    chain = ConversationalRetrievalChain.from_llm(
-        ChatOpenAI(model_name=model_name, temperature=0, streaming=True),
-        chain_type='stuff',
-        retriever=retriever,
-        memory=memory,
-        return_source_documents=True,
-    )
+    if chat_profile == 'GPT-4':
+        chain = gpt_chain('gpt-4o', retriever, memory)
+    elif chat_profile == 'GPT-3.5':
+        chain = gpt_chain('gpt-3.5-turbo', retriever, memory)
+    else:
+        chain = llama_chain('llama3.2:3b', retriever, memory)
 
-    await cl.Message(content='Agora você pode fazer perguntas!').send()
     cl.user_session.set('chain', chain)
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    # Recupera a cadeia de conversa armazenada na sessão do usuário
     chain = cl.user_session.get('chain')
-    
     if not chain:
         await message.reply('O sistema ainda não está pronto. Por favor, tente novamente mais tarde.')
         return
-
-    # Executa a cadeia com a mensagem do usuário
     response = await chain.ainvoke({'question': message.content})
-    
-    # Obtém a resposta e as fontes (se houver)
     answer = response['answer']
-    sources = response.get('source_documents', [])
-
-    # Envia a resposta para o usuário
     await cl.Message(content=answer).send()
+
+if not os.path.exists('vectorstore'):
+    print('Criando o vectorstore')
+    text_content = load_csv_data(PATH)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+    texts = text_splitter.split_text(text_content)
+    metadatas = [{'source': f'{i}-pl'} for i in range(len(texts))]
+    vectorstore = FAISS.from_texts(texts, OpenAIEmbeddings(), metadatas=metadatas)
+    vectorstore.save_local('vectorstore')
